ea1ef7360b915ee9157dce10467c68dfe72433cb
maga3 <72155333+maga3@users.noreply.github.com>
2022-03-03 11:26:48 +0100

    Documentacion

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.iescomercio.ed.bloque2.repaso.modelo","l":"Curso"},{"p":"com.iescomercio.ed.bloque2.repaso.modelo","l":"Persona"},{"p":"com.iescomercio.ed.bloque2.repaso.app","l":"Principal"}];updateSearchResults();
